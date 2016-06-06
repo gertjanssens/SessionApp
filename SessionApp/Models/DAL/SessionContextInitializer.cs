ea44e10241df92dfd777a2619703da5de310744d
andybusschots <andy.busschots@gmail.com>
2016-06-06 20:21:15 +0200

    fill seed

--- SessionApp/Models/DAL/SessionContextInitializer.cs
+++ SessionApp/Models/DAL/SessionContextInitializer.cs
@@ -6,11 +6,25 @@ using System.Text;
 
 namespace SessionApp.Models.DAL
 {
-    class SessionContextInitializer :DropCreateDatabaseIfModelChanges<SessionContext> 
+    public class SessionContextInitializer :DropCreateDatabaseIfModelChanges<SessionContext> 
     {
         protected override void Seed(SessionContext context)
         {
             base.Seed(context);
+
+            Session session1 = new Session();
+            session1.Id = 1;
+            session1.Name = "Asp.net core";//angular2 typescript
+
+            Session session2 = new Session();
+            session2.Id = 2;
+            session2.Name = "Anjular 2";//angular2 typescript
+
+            Session session3 = new Session();
+            session3.Id = 3;
+            session3.Name = "Typescript";//angular2 typescript
+
+            context.SaveChanges();
 
         }
     }
